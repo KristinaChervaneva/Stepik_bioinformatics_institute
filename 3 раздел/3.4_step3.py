2b9407dad4fcf0e27677144a6dbc1f9c9d9e5a12
KristinaChervaneva <chervaneva.kristina@gmail.com>
2023-08-08 12:13:51 +0300

    fjhgdflkgj

--- 3 раздел/3.4_step3.py
+++ 3 раздел/3.4_step3.py
@@ -13,15 +13,15 @@
 
 with open("input.txt", "r") as input_file, open("output.txt", "w") as output_file:
     text = input_file.read()
+    d = dict()
+    for i in range(len(text)):
+        if text[i] not in d:
+            d[text[i]] = text.count(text[i])
+    key_list = list(d.keys())
+    val_list = list(d.values())
 
-d = dict()
-for i in range(len(text)):
-    if text[i] not in d:
-        d[text[i]] = text.count(text[i])
-key_list = list(d.keys())
-val_list = list(d.values())
+    position = val_list.index(max(d.values()))
+    print(key_list[position], max(d.values()))
 
-position = val_list.index(max(d.values()))
-print(key_list[position], max(d.values()))
+    output_file.write("{} {}".format(key_list[position], max(d.values())))
 
-output_file.write("{} {}".format(key_list[position], max(d.values())))
